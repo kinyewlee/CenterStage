41e49f1c00200fb777002ada6bf107853574ca23
Kin Lee <mokyip@gmail.com>
2023-11-18 13:36:00 -0800

    Update away-from-backboard autonomous modes for the updated pixel holding mechanism

--- Team15091/src/main/java/org/firstinspires/ftc/team15091/RBPipeline.java
+++ Team15091/src/main/java/org/firstinspires/ftc/team15091/RBPipeline.java
@@ -1,68 +1,47 @@
 package org.firstinspires.ftc.team15091;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Rect;
-import org.opencv.core.Size;
+import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RBPipeline extends PipelineBase {
-    Mat temp = new Mat(), sub;
     String debug = "";
     Double[] hues = new Double[2];
     PixelPosition position = PixelPosition.Right;
 
     // Define region for left and middle
-    static final Rect maskLeft = new Rect(0, 190, 60, 50);
-    static final Rect maskMiddle = new Rect(320, 165, 60, 50);
+    static final List<Rect> rectRegions = Arrays.asList(
+            new Rect(0, 190, 60, 50),
+            new Rect(320, 165, 60, 50)
+    );
+    static final List<Point> circleCenters = Arrays.asList(
+            new Point(30, 215),
+            new Point(350, 185)
+    );
+    static final int radius = 25;
 
     @Override
     public Mat processFrame(Mat input) {
+        Mat temp = new Mat();
+
         // Convert to RBG to HSV, so we can extract HUE channel
         Imgproc.cvtColor(input, temp, Imgproc.COLOR_RGBA2RGB);
         Imgproc.cvtColor(temp, temp, Imgproc.COLOR_RGB2HSV);
-//        Imgproc.GaussianBlur(temp, temp, new Size(5, 5), 0);
 
-        sub = temp.submat(maskLeft);
-        hues[0] = Core.mean(sub).val[0];
-        sub.release();
-
-        sub = temp.submat(maskMiddle);
-        hues[1] = Core.mean(sub).val[0];
-        sub.release();
+        for (int i = 0; i < 2; i++) {
+            hues[i] = calculateMeanHueInCircle(temp, circleCenters.get(i), radius);
+            Imgproc.circle(input, circleCenters.get(i), radius, YELLOW, 2);
+        }
         temp.release();
-
-        // region Visual Aid
-        /*
-         * Draw a rectangle showing sample region 1,2 on the screen.
-         * Simply a visual aid. Serves no functional purpose.
-         */
-        Imgproc.rectangle(
-                input, // Buffer to draw on
-                maskLeft,
-                YELLOW, // The color the rectangle is drawn in
-                2); // Thickness of the rectangle lines
-        Imgproc.rectangle(
-                input, // Buffer to draw on
-                maskMiddle,
-                YELLOW, // The color the rectangle is drawn in
-                2); // Thickness of the rectangle lines
-                // endregion
 
         if (isRed(hues[0]) || isBlue(hues[0])) {
             position = PixelPosition.Left;
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    maskLeft, // Second point which defines the rectangle
-                    GREEN, // The color the rectangle is drawn in
-                    -1); // Negative thickness means solid fill
+            Imgproc.circle(input, circleCenters.get(0), radius, GREEN, -1);
         } else if (isRed(hues[1]) || isBlue(hues[1])) {
             position = PixelPosition.Middle;
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    maskMiddle, // Second point which defines the rectangle
-                    GREEN, // The color the rectangle is drawn in
-                    -1); // Negative thickness means solid fill
+            Imgproc.circle(input, circleCenters.get(1), radius, GREEN, -1);
         } else {
             position = PixelPosition.Right;
         }
@@ -71,11 +50,33 @@ public class RBPipeline extends PipelineBase {
         return input;
     }
 
-    boolean isRed(double hueValue) {
+    boolean isRed(Double hueValue) {
         return hueValue > 0 && hueValue < 20;
     }
 
-    boolean isBlue(double hueValue) {
+    boolean isBlue(Double hueValue) {
         return hueValue > 99 && hueValue < 120;
+    }
+
+    private static Double calculateMeanHueInCircle(Mat image, Point center, int radius) {
+        Mat mask = new Mat(image.size(), CvType.CV_8U, Scalar.all(0));
+        Imgproc.circle(mask, center, radius, new Scalar(255), -1);  // Draw a filled circle on the mask
+        Scalar meanColor = meanColorInCircle(image, mask);
+        mask.release();  // Release resources
+        return meanColor.val[0];
+    }
+
+    private static Scalar meanColorInCircle(Mat image, Mat mask) {
+        // Apply the mask to the image
+        Mat maskedImage = new Mat();
+        Core.bitwise_and(image, image, maskedImage, mask);
+
+        // Calculate the mean color using Core.mean
+        Scalar meanColor = Core.mean(maskedImage, mask);
+
+        // Release resources
+        maskedImage.release();
+
+        return meanColor;
     }
 }
