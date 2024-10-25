23b299cd1fe2d4675c5957d3abff1607aaed9af7
Kin Lee <mokyip@gmail.com>
2024-10-24 23:29:38 -0700

    First Draft

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadOpMode.java
@@ -0,0 +1,197 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp(name = "Gamepad")
+public class GamepadOpMode extends OpModeBase {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        double driveScale = 0.6;
+        int armLimit = 3500;
+        boolean loweringInProgress = false; // Initialize a flag to track lowering state
+        boolean raisingInProgress = false;
+        long liftStartTime = System.currentTimeMillis();
+
+        robot.init(hardwareMap);
+
+        //region telemetry setup
+        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.update();
+        //endregion
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            //region a button
+            if (gamepad1.a || gamepad2.a) {
+                if (!a_pressed) {
+                    a_pressed = true;
+                    robot.toggleSlide();
+                }
+            } else { // Reset the 'X' button press flag
+                a_pressed = false;
+            }
+            //endregion
+
+            //region a button
+            if (gamepad1.b || gamepad2.b) {
+                if (!b_pressed) {
+                    b_pressed = true;
+                    robot.intake();
+                }
+            } else { // Reset the 'X' button press flag
+                b_pressed = false;
+            }
+            //endregion
+
+            //region x button
+            if (gamepad1.x || gamepad2.x) {
+                if (!x_pressed) {
+                    x_pressed = true;
+                }
+            } else { // Reset the 'X' button press flag
+                x_pressed = false;
+            }
+            //endregion
+
+            //region arm control
+            int currentArmPosition = robot.liftMotor.getCurrentPosition();
+            boolean limitSwitch = robot.limitSwitch.getState();
+            if (gamepad1.left_trigger > 0d) { // raise lift
+                if (!limitSwitch) { // touched
+                }
+                robot.liftMotor.setTargetPosition(armLimit);
+                robot.setLiftMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.liftMotor.setPower(gamepad1.left_trigger);
+                loweringInProgress = false; // Reset the lowering state
+                raisingInProgress = false;
+            } else if (gamepad1.right_trigger > 0d &&  // lower lift
+                    limitSwitch) { // Check if the limit switch is not pressed
+                double powerScale = currentArmPosition > 800 ? 1d : 0.3d;
+                robot.setLiftMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+                robot.liftMotor.setPower(-gamepad1.right_trigger * powerScale);
+                loweringInProgress = false; // Reset the lowering state
+                raisingInProgress = false;
+            } else {
+                if (!loweringInProgress && !raisingInProgress) { // stop lift
+                    robot.liftMotor.setPower(0d);
+                } else if (loweringInProgress) {
+                    double elapsedTime = (System.currentTimeMillis() - liftStartTime) / 1000.0; // Convert to seconds
+                    robot.setLiftMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+                    robot.liftMotor.setPower(elapsedTime > 1d ? -0.8d : -0.1d);
+                }
+
+                if (!limitSwitch && !raisingInProgress) {
+                    if (loweringInProgress) {
+                        loweringInProgress = false;
+                    }
+                    robot.setLiftMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+                }
+            }
+            //endregion
+
+            //region left bumper
+            if (gamepad1.left_bumper || gamepad2.left_bumper) {
+                if (!lb_pressed) {
+                    lb_pressed = true;
+                    robot.setLiftMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    robot.liftMotor.setTargetPosition(armLimit);
+                    robot.setLiftMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    loweringInProgress = false;
+                    raisingInProgress = !raisingInProgress;
+                }
+            } else {
+                lb_pressed = false;
+                if (!raisingInProgress && !loweringInProgress && gamepad1.left_trigger == 0 && gamepad1.right_trigger == 0) { // stop lift
+                    robot.liftMotor.setPower(0d);
+                } else if (raisingInProgress) {
+                    robot.liftMotor.setPower(0.8);
+                }
+            }
+            //endregion
+
+            //region right bumper
+            if (gamepad1.right_bumper || gamepad2.right_bumper) {
+                if (!rb_pressed) {
+                    rb_pressed = true;
+                    loweringInProgress = !loweringInProgress;
+                    raisingInProgress = false;
+                    liftStartTime = System.currentTimeMillis();
+                }
+            } else {
+                rb_pressed = false;
+            }
+            //endregion
+
+            //region drivetrain control
+            double leftStickY = -gamepad1.left_stick_y;  // Invert if necessary
+            double rightStickY = -gamepad1.right_stick_y;  // Invert if necessary
+            double drive;
+
+            if (leftStickY >= 0 && rightStickY >= 0) {
+                // Both inputs are positive, so choose the maximum positive value.
+                drive = Math.max(leftStickY, rightStickY);
+            } else if (leftStickY <= 0 && rightStickY <= 0) {
+                // Both inputs are negative, so choose the minimum negative value.
+                drive = Math.min(leftStickY, rightStickY);
+            } else {
+                // The inputs have different signs, so set drive to the sum of both.
+                drive = leftStickY + rightStickY;
+            }
+
+            if (Math.abs(drive) > 0.5) {
+                // Adjust driveScale or perform other actions as needed
+                if (driveScale < 1.0) {
+                    driveScale += 0.01; // Adjust the increment value as needed
+                }
+            } else {
+                // Reset driveScale or perform other actions as needed when |drive| is not greater than 0.5
+                driveScale = 0.6;
+            }
+            // Apply the scaling factor (0.6 in this case):
+            drive *= driveScale;
+
+            double turn = gamepad1.left_stick_x * 0.6d;
+            double side = gamepad1.right_stick_x * 0.8d;;
+
+            double leftFrontPower = drive + turn + side;
+            double leftBackPower = drive + turn - side;
+            double rightFrontPower = drive - turn - side;
+            double rightBackPower = drive - turn + side;
+
+            // Normalize wheel powers to be less than 1.0
+            double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            if (max > 1.0) {
+                leftFrontPower /= max;
+                rightFrontPower /= max;
+                leftBackPower /= max;
+                rightBackPower /= max;
+            }
+
+            // Send calculated power to wheels
+            robot.setDrivePower(leftFrontPower, leftBackPower, rightFrontPower, rightBackPower);
+
+            // Show the elapsed game time and wheel power.
+            telemetry.clearAll();
+            telemetry.addLine("motor | ")
+                    .addData("lf", String.format("%.1f", leftFrontPower))
+                    .addData("lr", String.format("%.1f", leftBackPower))
+                    .addData("rf", String.format("%.1f", rightFrontPower))
+                    .addData("rr", String.format("%.1f", rightBackPower));
+            telemetry.addLine("sensor | ")
+                    .addData("limit", () -> String.format("%s", robot.limitSwitch.getState()));
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,221 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.content.Context;
+
+import com.qualcomm.ftccommon.SoundPlayer;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot.LogoFacingDirection;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot.UsbFacingDirection;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
+import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior;
+import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Robot {
+    public DcMotorEx leftFront, leftRear, rightRear, rightFront, liftMotor;
+    public Servo leftServo, rightServo, rollerServo, handServo;
+    public DigitalChannel limitSwitch;
+    private List<DcMotorEx> motors;
+    private Context _appContext;
+    private int[] beepSoundID = new int[3];
+    volatile boolean soundPlaying = false;
+    // create a sound parameter that holds the desired player parameters.
+    SoundPlayer.PlaySoundParams soundParams = new SoundPlayer.PlaySoundParams(false);
+
+    IMU imu;
+    private static final double MAX_VELOCITY = 2800d;
+    private static final double COUNTS_PER_MOTOR_REV = 529.2d;    // eg: HD Hex Motor 20:1 560, core hex 288, 40:1 1120
+    private static final double DRIVE_GEAR_REDUCTION = 1d;     // This is < 1.0 if geared UP, eg. 26d/10d
+    private static final double WHEEL_DIAMETER_INCHES = 2.953d;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.14159265359d);
+    double slidePosition, handPosition, rollerPosition;
+    private RunMode liftMode;
+
+    public void init(HardwareMap hardwareMap) {
+        leftFront = hardwareMap.get(DcMotorEx.class, "left_front");
+        leftRear = hardwareMap.get(DcMotorEx.class, "left_rear");
+        rightFront = hardwareMap.get(DcMotorEx.class, "right_front");
+        rightRear = hardwareMap.get(DcMotorEx.class, "right_rear");
+        motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront);
+
+        leftFront.setDirection(Direction.REVERSE);
+        leftRear.setDirection(Direction.FORWARD);
+        rightFront.setDirection(Direction.FORWARD);
+        rightRear.setDirection(Direction.FORWARD);
+
+        leftServo = hardwareMap.servo.get("servo_left");
+        rightServo = hardwareMap.servo.get("servo_right");
+        slidePosition = leftServo.getPosition();
+
+        rollerServo = hardwareMap.servo.get("servo_roller");
+        rollerPosition = rollerServo.getPosition();
+        handServo = hardwareMap.servo.get("servo_hand");
+        handPosition = handServo.getPosition();
+
+        limitSwitch = hardwareMap.get(DigitalChannel.class, "limit_sensor");
+        liftMotor = hardwareMap.get(DcMotorEx.class, "lift");
+        liftMotor.setDirection(Direction.FORWARD);
+        liftMotor.setCurrentAlert(1d, CurrentUnit.AMPS);
+        liftMotor.setPositionPIDFCoefficients(6d);
+        liftMotor.setTargetPositionTolerance(2);
+        liftMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        setLiftMode(RunMode.STOP_AND_RESET_ENCODER);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        imu.initialize(new IMU.Parameters(
+                new RevHubOrientationOnRobot(LogoFacingDirection.RIGHT, UsbFacingDirection.FORWARD)));
+        imu.resetYaw();
+
+        _appContext = hardwareMap.appContext;
+        beepSoundID[0] = hardwareMap.appContext.getResources().getIdentifier("beep", "raw", hardwareMap.appContext.getPackageName());
+        beepSoundID[1] = hardwareMap.appContext.getResources().getIdentifier("ss_laser", "raw", hardwareMap.appContext.getPackageName());
+        beepSoundID[2] = hardwareMap.appContext.getResources().getIdentifier("ss_bb8_up", "raw", hardwareMap.appContext.getPackageName());
+
+        setDriveZeroPowerBehavior(ZeroPowerBehavior.FLOAT);
+
+        beep();
+    }
+
+    public final void beep() {
+        beep(0);
+    }
+
+    final void beep(int beepType) {
+        if (!soundPlaying) {
+            soundPlaying = true;
+            SoundPlayer.getInstance().startPlaying(
+                    _appContext,
+                    beepSoundID[beepType],
+                    soundParams,
+                    null,
+                    () -> soundPlaying = false);
+        }
+    }
+
+    double getHeading() {
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        return orientation.getYaw(AngleUnit.DEGREES);
+    }
+
+    void resetDrive() {
+        setDriveMode(RunMode.STOP_AND_RESET_ENCODER);
+        setDriveMode(RunMode.RUN_USING_ENCODER);
+        setDriveZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+    }
+
+    void setDriveZeroPowerBehavior(ZeroPowerBehavior zeroPowerBehavior) {
+        for (DcMotorEx motor : motors) {
+            motor.setZeroPowerBehavior(zeroPowerBehavior);
+        }
+    }
+
+    public void setDrivePower(double pLeftFront, double pLeftRear, double pRightFront, double pRightRear) {
+        leftFront.setPower(pLeftFront);
+        leftRear.setPower(pLeftRear);
+        rightFront.setPower(pRightFront);
+        rightRear.setPower(pRightRear);
+    }
+
+    void setDriveVelocity(double pLeftFront, double pLeftRear, double pRightFront, double pRightRear) {
+        double vLeftFront = pLeftFront * MAX_VELOCITY;
+        double vLeftRear = pLeftRear * MAX_VELOCITY;
+        double vRightFront = pRightFront * MAX_VELOCITY;
+        double vRightRear = pRightRear * MAX_VELOCITY;
+        leftFront.setVelocity(vLeftFront);
+        leftRear.setVelocity(vLeftRear);
+        rightFront.setVelocity(vRightFront);
+        rightRear.setVelocity(vRightRear);
+    }
+
+    void setDriveMode(RunMode driveMode) {
+        for (DcMotorEx motor : motors) {
+            motor.setMode(driveMode);
+        }
+    }
+
+    void setDriveTarget(double distance, boolean moveSideway) {
+        // Determine new target position, and pass to motor controller
+        int moveCounts = (int) (distance * COUNTS_PER_INCH);
+
+        int dirFL = moveSideway ? -1 : 1;
+        int dirFR = 1;
+        int dirRL = 1;
+        int dirRR = moveSideway ? -1 : 1;
+
+        int leftFrontTarget = leftFront.getCurrentPosition() + moveCounts * dirFL;
+        int rightFrontTarget = rightFront.getCurrentPosition() + moveCounts * dirFR;
+        int leftRearTarget = leftRear.getCurrentPosition() + moveCounts * dirRL;
+        int rightRearTarget = rightRear.getCurrentPosition() + moveCounts * dirRR;
+
+        // Set Target and Turn On RUN_TO_POSITION
+        leftFront.setTargetPosition(leftFrontTarget);
+        rightFront.setTargetPosition(rightFrontTarget);
+        leftRear.setTargetPosition(leftRearTarget);
+        rightRear.setTargetPosition(rightRearTarget);
+    }
+
+    boolean isDriveBusy() {
+        return leftFront.isBusy() && leftRear.isBusy() &&
+                rightFront.isBusy() && rightRear.isBusy();
+    }
+
+    public void setSlidePosition(double newLeftPosition, double newRightPosition) {
+        if (slidePosition != newLeftPosition) {
+            slidePosition = newLeftPosition;
+            leftServo.setPosition(newLeftPosition);
+            rightServo.setPosition(newRightPosition);
+        }
+    }
+
+    public void setRollerPosition(double newRollerPosition) {
+        if (rollerPosition != newRollerPosition) {
+            rollerPosition = newRollerPosition;
+            rollerServo.setPosition(newRollerPosition);
+        }
+    }
+
+    public void toggleSlide() {
+        if (slidePosition == 1d) {
+            setSlidePosition(0d, 1d);
+        } else {
+            setSlidePosition(1d, 0d);
+            handServo.setPosition(1d);
+        }
+        setRollerPosition(0.5d);
+    }
+
+    public void intake() {
+        if (rollerPosition != 0.5d) {
+            setRollerPosition(0.5);
+        } else if (slidePosition == 1d) {
+            setRollerPosition(0d);
+        } else if (slidePosition == 0d) {
+            handServo.setPosition(0d);
+            setRollerPosition(1d);
+        }
+    }
+
+    public void setLiftMode(DcMotor.RunMode newMode) {
+        if (liftMode != newMode) {
+            liftMotor.setMode(newMode);
+            liftMode = newMode;
+        }
+    }
+}
